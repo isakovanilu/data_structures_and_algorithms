ea2482ab777260fc6774d9ff2ad0586476003250
Nilu Isakova <niluisakova@gmail.com>
2024-09-27 14:16:00 -0500

    added main file

---
+++ DS_W21/src/AddTwoNumbers.java
@@ -0,0 +1,54 @@
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum/10;
+
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            curr = curr.next;
+
+
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Test the addTwoNumbers method with two linked lists
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        AddTwoNumbers sln = new AddTwoNumbers();
+        ListNode result = sln.addTwoNumbers(l1, l2);
+
+        // Print the result
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+
+}
